d80a7df75e109c86f1e7d289118bd4d7ec266ef9
Flasky06 <bonnienjuguna106@gmail.com>
2023-03-23 19:20:14 +0300

    login page

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,42 +1,61 @@
-import React from "react";
-import useForm from "../../hooks/useForm";
+import React, { useState } from "react";
 
-function Login() {
-	const { values, handleChange, resetForm } = useForm({
-		name: "",
+const Login = () => {
+	const [showPassword, setShowPassword] = useState(false);
 
-		password: "",
-	});
-
-	function handleSubmit(event) {
-		event.preventDefault();
-		console.log(values);
-		resetForm();
-	}
+	const handleShowPassword = () => {
+		setShowPassword(!showPassword);
+	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				Name:
-				<input
-					type="text"
-					name="name"
-					value={values.name}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				Password:
-				<input
-					type="password"
-					name="password"
-					value={values.password}
-					onChange={handleChange}
-				/>
-			</label>
-			<button type="submit">Submit</button>
-		</form>
+		<div className="min-h-screen bg-gray-100 flex flex-col justify-center">
+			<div className="max-w-md mx-auto bg-white py-10 px-5 shadow-lg rounded-lg">
+				<h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
+					Login
+				</h2>
+				<form>
+					<div className="mb-4">
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="username"
+							type="text"
+							placeholder="Username"
+						/>
+					</div>
+					<div className="mb-6">
+						<div className="relative">
+							<input
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+								id="password"
+								type={showPassword ? "text" : "password"}
+								placeholder="Password"
+							/>
+							<span
+								className="absolute right-0 top-0 mt-3 mr-3 text-gray-500 cursor-pointer"
+								onClick={handleShowPassword}
+							>
+								{showPassword ? "Hide" : "Show"}
+							</span>
+						</div>
+					</div>
+					<div className="flex items-center justify-between">
+						<button
+							className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+							type="button"
+						>
+							Sign In
+						</button>
+					</div>{" "}
+					<a
+						className="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-800"
+						href="#"
+					>
+						Forgot Password?
+					</a>
+				</form>
+			</div>
+		</div>
 	);
-}
+};
 
 export default Login;
