0f4ef8271efc5835eabe25f65ad420aead987b2c
Flasky06 <bonnienjuguna106@gmail.com>
2023-03-24 02:41:08 +0300

    emergency contact form

---
+++ src/pages/EnrolmentForm/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import EnrolmentForm from "./Enrol";
+import PageHeader from "./PageHeader";
+
+function EnrollmentPage() {
+	return (
+		<>
+			<PageHeader />
+			<EnrolmentForm />
+		</>
+	);
+}
+
+export default EnrollmentPage;
