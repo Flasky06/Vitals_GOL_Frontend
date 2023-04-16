c7b0cf0630b896cfdddf2c2e5c4cc9f2d1f27573
codeme254 <otwomadennis365@gmail.com>
2023-04-16 04:44:25 -0400

    add quantity, start date and end date fields in the dosage reminder form

--- src/pages/DosageReminder/DosageForm.jsx
+++ src/pages/DosageReminder/DosageForm.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function DosageForm() {
   const [dosageName, setDosageName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [quantity, setQuantity] = useState("");
   const [dosageTimes, setDosageTimes] = useState([]);
   const [numOfDosageTimes, setNumOfDosageTimes] = useState("");
 
@@ -28,11 +32,13 @@ function DosageForm() {
     const dosageReminderInformation = {
       dosageName: dosageName,
       dosageTimes: dosageTimes,
+      startDate: startDate,
+      endDate: endDate,
+      quantity: quantity,
     };
     // console.log(dosageTimes);
     // console.log(dosageName);
     console.log(dosageReminderInformation);
-    // You can send the dosage times to the backend here
   };
 
   const handleDosageName = (event) => {
@@ -40,10 +46,26 @@ function DosageForm() {
     setDosageName(event.target.value);
   };
 
+  const handleStartDateChange = (event) => {
+    event.preventDefault();
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event) => {
+    event.preventDefault();
+    setEndDate(event.target.value);
+  };
+
+  const handleQuantityChange = (event) => {
+    event.preventDefault();
+    setQuantity(event.target.value);
+  };
+
   const dosageTimeInputs = dosageTimes.map((dosageTime, index) => (
     <div key={index} className="time-input-form-group mb-5 mr-5">
       <label className="font-bold capitalize">Dosage Time {index + 1}:</label>
       <input
+        required
         className="dosage-reminder-time p-3"
         type="time"
         value={dosageTime}
@@ -60,6 +82,7 @@ function DosageForm() {
             medicine name
           </label>
           <input
+            required
             className="p-2"
             type="text"
             name=""
@@ -69,11 +92,64 @@ function DosageForm() {
             placeholder="enter the name of the medicine"
           />
         </div>
+
+        {/* number of pills */}
+        <div className="dosage-reminder-form-group-main mb-3">
+          <label className="font-bold capitalize" htmlFor="quantity">
+            quantity
+          </label>
+          <input
+            required
+            className="p-2"
+            type="text"
+            name=""
+            id="quantity"
+            value={quantity}
+            onChange={handleQuantityChange}
+            placeholder="eg 1 spoonful"
+          />
+        </div>
+        {/* end of number of pills */}
+
+        {/* start date and end date */}
+        <div className="dosage-reminder-form-group-main mb-3">
+          <label className="font-bold capitalize" htmlFor="start-date">
+            start date
+          </label>
+          <input
+            required
+            className="p-2"
+            type="date"
+            name=""
+            id="start-date"
+            value={startDate}
+            onChange={handleStartDateChange}
+            placeholder="enter the start date of this dosage"
+          />
+        </div>
+
+        <div className="dosage-reminder-form-group-main mb-3">
+          <label className="font-bold capitalize" htmlFor="end-date">
+            end date
+          </label>
+          <input
+            required
+            className="p-2"
+            type="date"
+            name=""
+            id="end-date"
+            value={endDate}
+            onChange={handleEndDateChange}
+            placeholder="enter the end date of this dosage"
+          />
+        </div>
+        {/* end of start date and end date */}
         <div className="dosage-reminder-form-group-main mb-3">
           <label className="font-bold capitalize">
             Number of Dosage Times:
           </label>
           <input
+            required
             className="p-2"
             type="number"
             min={0}
@@ -84,12 +160,20 @@ function DosageForm() {
         </div>
       </div>
       <div className="all-time-inputs flex flex-wrap">{dosageTimeInputs}</div>
-      <button
-        type="submit"
-        className="dosage-input-submit font-bold uppercase block p-3"
-      >
-        Submit dosage information
-      </button>
+      <div className="flex flex-col items-center mt-5">
+        <button
+          type="submit"
+          className="dosage-input-submit font-bold uppercase inline-block p-3"
+        >
+          Submit dosage information
+        </button>
+        <Link
+          to="/hospital/patient-files/all"
+          className="dosage-input-clear mt-2 font-bold uppercase inline-block p-3"
+        >
+          cancel
+        </Link>
+      </div>
     </form>
   );
 }
