cb997d5c30aa9cda13173b72b57850978ce4aa97
Flasky06 <bonnienjuguna106@gmail.com>
2023-04-28 03:14:37 +0300

    a

--- src/pages/LandingPage/Features/Features.jsx
+++ src/pages/LandingPage/Features/Features.jsx
@@ -8,44 +8,44 @@ import patientFile from "../../../assets/images/patient_file.jpg";
 import insurance from "../../../assets/images/insurance1.jpg";
 
 const Features = () => {
-  return (
-    <section className="features mt-24" id="features">
-      <TitleElement
-        shouldCenter={true}
-        mainTitle="what vitals brings to the table"
-        subTitle="features"
-      />
-      <div className="features__container p-10">
-        <Feature
-          image={pregnant}
-          title="maternal system"
-          description="The maternal system is an online platform that stores comprehensive information on pregnant women. It allows healthcare professionals to access and update patient records, including medical history, test results, and appointments. The system ensures secure storage and easy retrieval of data, enhancing maternal healthcare and reducing medical errors. Patients can also access their information, monitor their progress, and communicate with their healthcare providers."
-          link="/login"
-        />
+	return (
+		<section className="features mt-24" id="features">
+			<TitleElement
+				shouldCenter={true}
+				mainTitle="what vitals brings to the table"
+				subTitle="features"
+			/>
+			<div className="features__container p-10">
+				<Feature
+					image={pregnant}
+					title="maternal care"
+					description="The maternal system is an online platform that stores comprehensive information on pregnant women. It allows healthcare professionals to access and update patient records, including medical history, test results, and appointments. The system ensures secure storage and easy retrieval of data, enhancing maternal healthcare and reducing medical errors. Patients can also access their information, monitor their progress, and communicate with their healthcare providers."
+					link="/login"
+				/>
 
-        <Feature
-          image={dosage}
-          title="dosage reminder system"
-          description="This online system reminds users to take medication at scheduled times, via email or SMS. It helps users manage their medication regimen, promoting good health and reducing medication-related issues. The system may also include features such as tracking adherence, providing medication education, and allowing users to share their schedules with healthcare providers or family members for additional support."
-          link="/login"
-        />
+				<Feature
+					image={dosage}
+					title=" reminder"
+					description="This online system reminds users to take medication at scheduled times, via email or SMS. It helps users manage their medication regimen, promoting good health and reducing medication-related issues. The system may also include features such as tracking adherence, providing medication education, and allowing users to share their schedules with healthcare providers or family members for additional support."
+					link="/login"
+				/>
 
-        <Feature
-          image={patientFile}
-          title="patient files online"
-          description="This system enables the secure storage and easy retrieval of patient files online. It allows healthcare providers to access and update patient records, including medical history, test results, and treatment plans, from any location. Patients can also access their information, monitor their progress, and communicate with their healthcare providers. With this system, medical errors are reduced, and patient care is enhanced."
-          link="/login"
-        />
+				<Feature
+					image={patientFile}
+					title="online filing"
+					description="This system enables the secure storage and easy retrieval of patient files online. It allows healthcare providers to access and update patient records, including medical history, test results, and treatment plans, from any location. Patients can also access their information, monitor their progress, and communicate with their healthcare providers. With this system, medical errors are reduced, and patient care is enhanced."
+					link="/login"
+				/>
 
-        <Feature
-          image={insurance}
-          title="insurance info system"
-          description="This system educates users about various medical insurance schemes, offering comprehensive information about coverage options, premium rates, and claims management. Users can make informed decisions when selecting an insurance plan that suits their needs and stay up-to-date with changes in policies. With this system, users can access important information about medical insurance and ensure appropriate coverage for their healthcare needs."
-          link="/health-insurance-landing"
-        />
-      </div>
-    </section>
-  );
+				<Feature
+					image={insurance}
+					title="insurance info "
+					description="This system educates users about various medical insurance schemes, offering comprehensive information about coverage options, premium rates, and claims management. Users can make informed decisions when selecting an insurance plan that suits their needs and stay up-to-date with changes in policies. With this system, users can access important information about medical insurance and ensure appropriate coverage for their healthcare needs."
+					link="/health-insurance-landing"
+				/>
+			</div>
+		</section>
+	);
 };
 
 export default Features;
